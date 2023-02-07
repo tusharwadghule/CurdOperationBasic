7e4d2216ed03aafb01d475f533ce83812861b6b5
tushar <tusharwadghule@gmail.com>
2023-02-07 10:42:49 +0530

    Ticket-01 is my first ticket
    I have created my new branch and push the changes against it

---
+++ src/main/java/com/FirstProject/CrudoperationBasic/CrudoperationBasicApplication.java
@@ -0,0 +1,13 @@
+package com.FirstProject.CrudoperationBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudoperationBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudoperationBasicApplication.class, args);
+	}
+
+}
